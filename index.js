f8efc581680383e8485cb16731a1d3c228ac0aef
mdnasiruddin78 <nasiruddinanik54@gmail.com>
2024-12-26 02:26:08 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
@@ -26,7 +26,7 @@ async function run() {
         const reviewCollection = client.db("serviceDb").collection("reviewInfo");
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         // all service data to database post
         app.post('/addService', async (req, res) => {
@@ -135,7 +135,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
